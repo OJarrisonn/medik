f27dc44340e6e97d0759eb7237a944e7c9824b8b
OJarrisonn <j.h.m.t.v.10@gmail.com>
2025-01-02 19:39:23 -0300

    test(cmd): test loadEnv function
    Signed-off-by: OJarrisonn <j.h.m.t.v.10@gmail.com>

--- pkg/cmd/root_test.go
+++ pkg/cmd/root_test.go
@@ -3,12 +3,72 @@ package cmd
 import (
 	"os"
 	"testing"
+
+	"github.com/OJarrisonn/medik/pkg/medik"
 )
+
+func TestLoadEnvFileNotSet(t *testing.T) {
+	medik.EnvFile = ""
+
+	_, err := loadEnv()
+	if err != nil {
+		t.Errorf("loadEnv() not accepted empty filename: %v", err)
+	}
+}
+
+func TestLoadEnvFileInexistent(t *testing.T) {
+	medik.EnvFile = "/this/file/is/inexistent.env"
+
+	_, err := loadEnv()
+	if err == nil {
+		t.Error("loadEnv() accepted an non existent file")
+	}
+}
+
+func TestLoadEnvFile(t *testing.T) {
+	medik.EnvFile = "../../samples/root_test.env"
+
+	_, err := loadEnv()
+	if err != nil {
+		t.Errorf("loadEnv() failed: %v", err)
+	}
+
+	root, ok := os.LookupEnv("ROOT")
+
+	if !ok {
+		t.Error("loadEnv() failed: ROOT not set")
+	}
+
+	if root != "test" {
+		t.Errorf("loadEnv() failed: ROOT = '%v'", root)
+	}
+
+	test, ok := os.LookupEnv("TEST")
+
+	if !ok {
+		t.Error("loadEnv() failed: TEST not set")
+	}
+
+	if test != "root" {
+		t.Errorf("loadEnv() failed: TEST = '%v'", test)
+	}
+
+	os.Unsetenv("TEST")
+	os.Unsetenv("ROOT")
+}
 
 func TestUseEmptyEnv(t *testing.T) {
 	_, err := useEnv("")
 	if err != nil {
 		t.Errorf("useEnv() failed: %v", err)
+	}
+}
+
+func TestUseEnvWithInvalid(t *testing.T) {
+	ok, err := useEnv("FOO")
+
+	if err == nil || ok {
+		t.Errorf("useEnv() accepted invalid env file: %v", err)
 	}
 }
 
